3b6413edf84011788fe0e8d30d742e1e81c136c6
dianhsu <xudian.cn@gmail.com>
2021-06-10 21:19:40 +0800

    update

--- project/data_selection.h
+++ project/data_selection.h
@@ -87,3 +87,30 @@ void arrange_data(T* input, T* output,
         }
     }
 }
+/**
+ * @brief 将小矩阵安置到大矩阵中，支持循环安置
+ * @param input 输入地址的基址
+ * @param output 输出地址的基址
+ * @param dim1 输入数据第一维大小
+ * @param dim2 输入数据第二维大小
+ * @param dim3 输入数据第三维大小
+ * @param sz1 选择的数据第一维大小
+ * @param sz2 选择的数据第二维大小
+ * @param sz3 选择的数据第三维大小
+ * @param off1 选择的数据第一维偏移量大小
+ * @param off2 选择的数据第二维偏移量大小
+ * @param off3 选择的数据第三维偏移量大小
+ * */
+template<typename T>
+void arrange_data_roll(T* input, T* output,
+        int dim1, int dim2, int dim3, 
+        int sz1, int sz2, int sz3, 
+        int off1, int off2, int off3){
+    for(int i = 0; i < sz1; ++i){
+        for(int j = 0; j < sz2; ++j){
+            for(int k = 0; k < sz3; ++k){
+                output[(sz1 + off1 + dim1) % dim1 * dim2 * dim3 + (sz2 + off2 + dim2) % dim2 * dim3 + (sz3 + off3 + dim3) % dim3]=input[i * sz2 * sz3 + j * sz3 + k];
+            }
+        }
+    }
+}
--- project/swin_block.h
+++ project/swin_block.h
@@ -1,1 +1,76 @@
 #include "linear.h"
+#include "data_selection.h"
+#include "functions.h"
+
+/**
+ * @brief 计算整块内容
+ * @param input 输入基址
+ * @param output 输出基址
+ * @param param 参数基址
+ * @param id1 第一维宽度
+ * @param id2 第二维宽度
+ * @param ic3 通道数
+ * @param ws window的大小
+ * @param heads 多头的数量
+ * */
+template<typename T>
+void swin_block(T* input, T* param, T* output, int id1, int id2, int ic3, int ws, int heads) {
+    T tmp1[100000];
+    for (int i = 0; i < id1 * id2 * ic3; ++i) {
+        tmp1[i] = input[i];
+    }
+    T tmp[100000];
+    int c = ic3 / heads;
+    for (int i = 0; i < id1; ++i) {
+        for (int j = 0; j < id2; ++j) {
+            select_data<T>(input, tmp, id1, id2, ic3, 1, 1, ic3, i, j, 0);
+            layer_normalization<T>(tmp, tmp, ic3, param[0], param[1], param[2]); // TODO: 修改这个部分参数
+            arrange_data<T>(tmp, input, id1, id2, ic3, 1, 1, ic3, i, j, 0);
+        }
+    }
+    int qkv[3][100000];
+    int tmp2[10000];
+    int tmp3[3][10000];
+    for (int i = 0; i < id1; i += ws) {
+        for (int j = 0; j < id2; j += ws) {
+            for (int i1 = 0; i1 < ws; ++i1) {
+                for (int j1 = 0; j1 < ws; ++j1) {
+                    select_data<T>(input, tmp, id1, id2, ic3, 1, 1, ic3, i + i1, j + j1, 0);
+                    for (int kase = 0; kase < 3; ++kase) {
+                        linear<T>(tmp1, tmp2, param, ic3, ic3);
+                        arrange_data<T>(tmp2, qkv[kase], ws, ws, ic3, 1, 1, ic3, i1, j1, 0);
+                    }
+                }
+            }
+            for (int k = 0; k < ic3; k += c) {
+                select_data<T>(qkv[0], tmp3[0], ws, ws, ic3, ws, ws, c, 0, 0, k);
+                // TODO: ?!?! 小立方体压缩相乘
+
+                arrange_data<T>(tmp3, input, id1, id2, ic3, ws, ws, c, i, j, k);
+            }
+        }
+    }
+    for (int i = 0; i < id1 * id2 * ic3; ++i) {
+        output[i] += input[i];
+    }
+}
+
+
+/**
+ * @brief 计算整块内容
+ * @param input 输入基址
+ * @param output 输出基址
+ * @param param 参数基址
+ * @param id1 第一维宽度
+ * @param id2 第二维宽度
+ * @param ic3 通道数
+ * @param ws window的大小
+ * @param heads 多头的数量
+ * @param sf shift的大小
+ * */
+template<typename T>
+void swin_block_shift(T* input, T* param, T* output, int id1, int id2, int ic3, int ws, int heads, int sf) {
+    for (int i = 0; i < id1 * id2 * ic3; ++i) {
+        output[i] = input[i];
+    }
+}
