0e7eda6a0e22ca5ba0a60a0644720bc91168f317
dianhsu <xudian.cn@gmail.com>
2021-06-22 00:59:40 +0800

    update

--- cpu/include/controller.h
+++ cpu/include/controller.h
@@ -4,11 +4,36 @@
 
 #ifndef STP_CONTROLLER_H
 #define STP_CONTROLLER_H
-#include <iostream>
-unsigned regs[32];
-template<typename T>
-void Control(std::istream& c_data, T* input, T* output, std::istream& param){
 
+#include <iostream>
+
+unsigned regs[32];
+
+template<typename T>
+void control(std::istream &din, T *input, T *output, std::istream &param) {
+    unsigned c;
+    while (din >> c) {
+        unsigned x;
+        switch (c) {
+            case 0:
+                // 批量修改寄存器
+                din >> x;
+                for (unsigned i = 0; i < x; ++i) {
+                    din >> regs[i];
+                }
+                break;
+            case 1:
+                // 修改单个寄存器
+                din >> x;
+                din >> regs[x];
+                break;
+            case 2:
+
+                break;
+            default:
+                return;
+        }
+    }
 }
 
 #endif //STP_CONTROLLER_H
