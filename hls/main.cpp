e42cd816e3da6c90404f6c5d07c4d3154400e2d0
dianhsu <xudian.cn@gmail.com>
2021-08-18 10:29:57 +0800

    update

--- hls/main.cpp
+++ hls/main.cpp
@@ -1,6 +1,15 @@
 #include "main.h"
 
 void project_top(data_t* basePtr, ptr_stream& cIns, param_stream& pIns) {
-    control<data_t>(basePtr, cIns, pIns);
+    //control<data_t>(basePtr, cIns, pIns);
     //matrix<data_t>(basePtr, pIns);
+    regs[9] = 2;
+    regs[1] = 96;
+    regs[2] = 56;
+    regs[3] = 56;
+    regs[4] = 192;
+    regs[5] = 28;
+    regs[6] = 28;
+    regs[55] = 0;
+    partition_merge<T>(basePtr, pIns);
 }
